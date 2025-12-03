7abd9fb0ab7480d0cd2e9ad6758452932bc7fbb4
DanielCortizo23 <a01715091@tec.mx>
2025-12-03 08:31:20 -0600

    Update Partido.h

--- Partido.h
+++ Partido.h
@@ -1,11 +1,20 @@
-#ifndef PARTIDO_H
-#define PARTIDO_H
+/**
+ * Clase Partido
+ * Maneja los dos equipos que participan y el marcador del partido.
+ */
+
+#ifndef PARTIDO_H_
+#define PARTIDO_H_
 
 #include <iostream>
 #include "Equipo.h"
 using namespace std;
 
+/**
+ * Clase Partido
+ */
 class Partido {
+
 protected:
     Equipo equipoA;
     Equipo equipoB;
@@ -13,15 +22,42 @@ protected:
     int puntosB;
 
 public:
+
+    /**
+     * Constructor por default
+     *
+     * @param
+     * @return Objeto Partido
+     */
     Partido() : puntosA(0), puntosB(0) {}
 
+    /**
+     * Constructor con parámetros
+     *
+     * @param a Equipo A
+     * @param b Equipo B
+     * @return Objeto Partido
+     */
     Partido(Equipo a, Equipo b) : equipoA(a), equipoB(b), puntosA(0), puntosB(0) {}
 
+    /**
+     * Modifica el marcador del partido
+     *
+     * @param a Puntos del equipo A
+     * @param b Puntos del equipo B
+     * @return
+     */
     void setMarcador(int a, int b) {
         puntosA = a;
         puntosB = b;
     }
 
+    /**
+     * Muestra el marcador final
+     *
+     * @param
+     * @return
+     */
     void mostrarMarcador() {
         cout << "--- Marcador Final ---\n";
         cout << equipoA.get_nombre() << ": " << puntosA << endl;
@@ -30,3 +66,5 @@ public:
 };
 
 #endif
+
+
