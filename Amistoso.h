ec03bf0841f2003392806108c2b0020775eda295
DanielCortizo23 <a01715091@tec.mx>
2025-12-03 08:22:58 -0600

    Update Amistoso.h

--- Amistoso.h
+++ Amistoso.h
@@ -1,3 +1,16 @@
+/*
+ * Clase: Amistoso
+ * Descripción:
+ * Representa un partido de tipo amistoso dentro del sistema.
+ * Hereda de la clase Partido y agrega un atributo adicional:
+ * - motivo: razón por la cual se realiza el encuentro.
+ *
+ * Esta clase demuestra el uso de:
+ * - Herencia (extiende a Partido)
+ * - Constructores que inicializan tanto la parte base como la parte derivada
+ * - Sobrescritura de comportamiento (mostrarInfo)
+ */
+
 #ifndef AMISTOSO_H
 #define AMISTOSO_H
 
@@ -7,19 +20,44 @@
 using namespace std;
 
 class Amistoso : public Partido {
-private:
+private
+
+    // Motivo del partido amistoso
     string motivo;
 
 public:
+
+    /**
+     * Constructor por default.
+     *
+     * @param
+     * @return Objeto Amistoso
+     */
     Amistoso(): motivo("") {}
+
+    /**
+     * Constructor que recibe los equipos y el motivo.
+     *
+     * @param a Equipo 1
+     * @param b Equipo 2
+     * @param m Motivo del partido amistoso
+     * @return Objeto Amistoso
+     */
     Amistoso(Equipo a, Equipo b, string m) : Partido(a, b), motivo(m) {}
 
+        /**
+     * Muestra la información del partido amistoso.
+     *
+     * @param
+     * @return
+     */
     void mostrarInfo() {
         cout << "Partido amistoso - Motivo: " << motivo << endl;
     }
 };
 
 #endif
+
 
 
 
