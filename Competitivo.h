e6d4598ebb31d9ea3b0ad5ef1a5224c3543d0aeb
DanielCortizo23 <a01715091@tec.mx>
2025-12-03 08:24:59 -0600

    Update Competitivo.h

--- Competitivo.h
+++ Competitivo.h
@@ -1,3 +1,8 @@
+/*
+ * Clase Competitivo
+ * Representa un partido competitivo que hereda de Partido.
+ * Agrega la sede y el premio del partido.
+ */
 #ifndef COMPETITIVO_H
 #define COMPETITIVO_H
 
@@ -8,14 +13,41 @@ using namespace std;
 
 class Competitivo : public Partido {
 private:
+
+    // Lugar donde se realiza el partido competitivo
     string sede;
+
+    // Premio económico del partido
     int premio;
 
 public:
+
+    /**
+     * Constructor por default.
+     *
+     * @param
+     * @return Objeto Competitivo
+     */
     Competitivo() : sede(""), premio(0) {}
+  
+    /**
+     * Constructor que recibe equipos, sede y premio.
+     *
+     * @param a Equipo 1
+     * @param b Equipo 2
+     * @param s Sede del partido
+     * @param p Premio económico
+     * @return Objeto Competitivo
+     */
     Competitivo(Equipo a, Equipo b, string s, int p)
         : Partido(a, b), sede(s), premio(p) {}
 
+        /**
+     * Muestra la información del partido competitivo.
+     *
+     * @param
+     * @return
+     */
     void mostrarInfo() {
         cout << "Partido competitivo en " << sede
              << " por $" << premio << endl;
@@ -23,3 +55,4 @@ public:
 };
 
 #endif
+
